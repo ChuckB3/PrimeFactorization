156b1ee6b7df45a7f65c94c1e0955e2596e4bb55
ChuckB3 <cbrumbaugh3@gmail.com>
2017-04-21 18:12:59 -0400

    Import Project

---
+++ PrimeFactorization/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PrimeFactorization
+{
+    partial class PrimeFactorization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find the prime factors of:";
+            // 
+            // numBox
+            // 
+            this.numBox.Location = new System.Drawing.Point(80, 39);
+            this.numBox.Name = "numBox";
+            this.numBox.Size = new System.Drawing.Size(100, 20);
+            this.numBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PrimeFactorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 119);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numBox);
+            this.Controls.Add(this.label1);
+            this.Name = "PrimeFactorization";
+            this.ShowIcon = false;
+            this.Text = "Prime Factorization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PrimeFactorization/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrimeFactorization
+{
+    public partial class PrimeFactorization : Form
+    {
+        public long num;
+        public int n;        
+        public List<String> primeFacts = new List<String>();
+        public PrimeFactorization()
+        {
+            InitializeComponent();
+        }
+        
+       
+        private void primeFact(long n)
+        {
+            for( int i = 2; i <= n; i++)
+            {
+                while(n%i==0)
+                {                    
+                    n /= i;
+                    primeFacts.Add(i.ToString());
+                }                               
+            }         
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            primeFacts.Clear();
+            num = long.Parse(numBox.Text);
+            primeFact(num);
+            var results = String.Join(", ", primeFacts.ToArray());
+            MessageBox.Show(results, "Results");
+
+        }
+    }
+}
